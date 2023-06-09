5aec5f627cf274e86308d4f3f0b1c8c3dbd88f7c
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2023-06-10 00:15:28 +0530

    Deleting Backend directory due to repository error
    The repository shows 02 separate backend folders, therefore the author of this repo will delete both dirs. and re-push the files from the last commit

--- Backend/Backend.Contracts/TodoTask/CreateTaskRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace Backend.Contracts.TodoTask;
-
-public record CreateTaskRequest(
-    string Title,
-    string? Description,
-    DateTime DueDate,
-    bool IsCompleted,
-    string Priority);
--- Backend/Backend.Contracts/TodoTask/TaskResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend.Contracts.TodoTask;
-
-public record TaskResponse(
-    Guid Id,
-    string Name,
-    string? Description,
-    DateTime DueDate,
-    bool IsCompleted,
-    int Priority);
